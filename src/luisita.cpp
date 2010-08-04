14b3713598efdac4499e3b0d3a83af89ca9983dd
sole <listas@soledadpenades.com>
2010-08-05 02:45:02 +0300

    remove ntsc and dxt flags when loading images

--- src/luisita.cpp
+++ src/luisita.cpp
@@ -736,7 +736,7 @@ int luisita_gfxLoadImage(const char* filename)
 	 filename,
 	 SOIL_LOAD_AUTO,
 	 SOIL_CREATE_NEW_ID,
-	 SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+	 SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y
 	 );
 	
 	if(textureId == 0)
