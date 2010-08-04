b76e54867f8621a7500e376fda8229fa39939404
sole <listas@soledadpenades.com>
2010-08-04 11:13:18 +0100

    32/64 builds plus audio functions and co

--- src/luisita.cpp
+++ src/luisita.cpp
@@ -5,6 +5,8 @@
 #include <cmath>
 #include <vector>
 
+#include "bass/bass.h"
+
 enum
 {
     STATUS_NEEDS_LOADING,
@@ -26,19 +28,49 @@ static lua_State *L;
 int status;
 int status_shouldDraw;
 
+// Music/Audio
+std::vector<HSTREAM> musicStreams;
+
+// GFX
+GLfloat screenBackground[4];
+
+// ---------------------------------
+
+
 int luisita_init()
 {
     int i;
-
+	
     status_shouldDraw = 1;
     status = STATUS_NEEDS_LOADING;
+    
+    if (!BASS_Init(-1, 44100,0,0,NULL))
+	{
+		printf("Music device couldn't be initialized (error code = %d)\n", BASS_ErrorGetCode());
+		return(0);
+	}
 
     L = lua_open();
     luaL_openlibs(L);
 
     lua_register(L, "trace", luisita_luaTrace);
+    
+    lua_register(L, "windowTitle", luisita_luaWindowTitle);
+    lua_register(L, "setupIsDone", luisita_luaSetupIsDone);
+	lua_register(L, "quit", luisita_luaQuit);
 
     lua_register(L, "loadShader", luisita_luaLoadShader);
+    lua_register(L, "background", luisita_luaBackground);
+    
+    lua_register(L, "loadMusicStream", luisita_luaLoadMusicStream);
+	lua_register(L, "playMusicStream", luisita_luaPlayMusicStream);
+	lua_register(L, "isMusicStreamFinished", luisita_luaIsMusicStreamFinished);
+	lua_register(L, "getMusicStreamTime", luisita_luaGetMusicStreamTime);
+
+	screenBackground[0] = 0.3f;
+	screenBackground[1] = 0.3f;
+	screenBackground[2] = 0.3f;
+	screenBackground[3] = 1.0f;
 
     return 1;
 }
@@ -46,6 +78,13 @@ int luisita_init()
 int luisita_end()
 {
     int i;
+
+	for(i = 0; i < musicStreams.size(); i++)
+	{
+		BASS_StreamFree(musicStreams.at(i));
+	}
+
+	BASS_Free();
 
     if (L)
     {
@@ -58,7 +97,7 @@ int luisita_end()
 void luisita_run()
 {
     SDL_Event event;
-    printf("luisita_run\n");
+    
     while (!luisita_hasFinished())
     {
         luisita_render();
@@ -111,16 +150,38 @@ int luisita_hasFinished()
 
 void luisita_render()
 {
-    // does the draw function exist in lua space? if so, call it with current time
-    lua_getglobal(L, "draw");
-    if (lua_isfunction(L, 1))
-    {
-        lua_pushnumber(L, SDL_GetTicks());
-        lua_call(L, 1, 0);
-    }
-    else
-    {
-        lua_pop(L, 1);
+	if(status == STATUS_NEEDS_LOADING)
+	{
+		// does the setup function exist in lua space?
+		lua_getglobal(L, "setup");
+		if(lua_isfunction(L, 1))
+		{
+			lua_call(L, 0, 0);
+			// setup should call swap_buffers via progress bar and stay there until it finishes :)
+		}
+		else
+		{
+			// end of story!
+			status = STATUS_LOADED;
+		}
+	}
+	
+	if(status == STATUS_LOADED)
+	{
+		//glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		//glClear(GL_COLOR_BUFFER_BIT);
+		
+		// does the draw function exist in lua space? if so, call it with current time
+		lua_getglobal(L, "draw");
+		if (lua_isfunction(L, 1))
+		{
+		    lua_pushnumber(L, SDL_GetTicks());
+		    lua_call(L, 1, 0);
+		}
+		else
+		{
+		    lua_pop(L, 1);
+		}
     }
 
     SDL_GL_SwapBuffers();
@@ -149,10 +210,36 @@ int luisita_luaTrace(lua_State *L)
     return 0;
 }
 
+// Misc functions
+// ``````````````
+
+int luisita_luaWindowTitle(lua_State *L)
+{
+	const char *text = lua_tostring(L, 1);
+	SDL_WM_SetCaption(text, NULL);
+	
+	return 0;
+}
+
+int luisita_luaSetupIsDone(lua_State *L)
+{
+	// When setup is done, give some time for the system to 'relax'
+	sleep(3);
+	status = STATUS_LOADED;
+	return 0;
+}
+
+int luisita_luaQuit(lua_State *L)
+{
+	status = STATUS_FINISHED;
+	return 0;
+}
+
 
 // GFX functions
 // ``````````````
 
+// NOTE this obviously is not finished yet!!! someday I'll have time to look into shaders...
 static int luisita_luaLoadShader(lua_State *L)
 {
     const char *shaderFileName;
@@ -211,4 +298,80 @@ Parameters:
 
 
     return 0;
+}
+
+int luisita_luaBackground(lua_State *L)
+{
+	int i;
+	for(i = 0; i < 4; i++)
+	{
+		screenBackground[i] = lua_tonumber(L, i+1);
+	}
+	glClearColor(screenBackground[0], screenBackground[1], screenBackground[2], screenBackground[3]);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	return 0;	 
+}
+
+// AUDIO functions
+// ```````````````
+
+int luisita_luaLoadMusicStream(lua_State *L)
+{
+	const char *path = lua_tostring(L, 1);
+
+	HSTREAM stream = BASS_StreamCreateFile(FALSE, path, 0, 0, BASS_STREAM_PRESCAN);
+
+	if(stream)
+	{
+		musicStreams.push_back(stream);
+		lua_pushinteger(L, musicStreams.size() - 1);
+	}
+	else
+	{
+		printf("Can't load stream file %s (error code = %d)\n", path, BASS_ErrorGetCode());		
+		lua_pushnil(L);
+	}
+
+	return 1;
+}
+
+int luisita_luaPlayMusicStream(lua_State *L)
+{
+	float positionMillis = 0;
+	int index = lua_tointeger(L, 1);
+
+	if(lua_gettop(L) == 2)
+	{
+		positionMillis = lua_tonumber(L, 2);
+	}
+
+	if(index + 1 <= musicStreams.size())
+	{
+		BASS_ChannelPlay(musicStreams.at(index), FALSE);
+		if(positionMillis > 0)
+		{
+			BASS_ChannelSetPosition(musicStreams.at(index), BASS_ChannelSeconds2Bytes(musicStreams.at(index), positionMillis / 1000), BASS_POS_BYTE);
+		}
+	}
+	return 0;
+}
+
+int luisita_luaIsMusicStreamFinished(lua_State *L)
+{
+	int stream = musicStreams.at(lua_tointeger(L, 1));
+	QWORD pos = BASS_ChannelGetPosition(stream, BASS_POS_BYTE);
+	QWORD len = BASS_ChannelGetLength(stream, BASS_POS_BYTE);
+
+	lua_pushboolean(L, pos >= len);
+
+	return 1;
+}
+
+int luisita_luaGetMusicStreamTime(lua_State *L)
+{
+	// This value is the current position of the stream in SECONDS
+	int channel = musicStreams.at(lua_tointeger(L, 1));
+	lua_pushnumber(L, BASS_ChannelBytes2Seconds(channel, BASS_ChannelGetPosition(channel, BASS_POS_BYTE)) );
+	return 1;
 }
--- src/luisita.h
+++ src/luisita.h
@@ -18,9 +18,21 @@ void luisita_reportErrors(lua_State *L, int status);
 // logging functions
 static int luisita_luaTrace(lua_State *L);
 
+// misc functions
+static int luisita_luaWindowTitle(lua_State *L);
+static int luisita_luaSetupIsDone(lua_State *L);
+static int luisita_luaQuit(lua_State *L);
+
 // gfx functions
 static int luisita_luaLoadShader(lua_State *L);
- 
+static int luisita_luaBackground(lua_State *L);
+
+// audio functions
+static int luisita_luaLoadMusicStream(lua_State *L);
+static int luisita_luaPlayMusicStream(lua_State *L);
+static int luisita_luaIsMusicStreamFinished(lua_State *L);
+static int luisita_luaGetMusicStreamTime(lua_State *L);
+
 #endif
 
 
